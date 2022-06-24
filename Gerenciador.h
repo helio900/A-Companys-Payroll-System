bcc02b9d73340f939ed65cbd68eccc6c0273a95b
Hélio II <105680383+helio900@users.noreply.github.com>
2022-06-24 13:31:24 -0300

    Update Gerenciador.h

--- Gerenciador.h
+++ Gerenciador.h
@@ -18,10 +18,10 @@ class Gerenciador : public Funcionario
         void exibeTipoFuncionario(int d);
         void aumentaSalario();
         void setFolhaDePagamento(int mes);
-        void getFolhaDePagamento(int mes);
+        //void getFolhaDePagamento(int mes);
         int getMesDeAumento();
-        void getFolhaFuncionarioCod(int cod);
-        void getFolhaFuncionarioNome(std::string n);
+        //void getFolhaFuncionarioCod(int cod);
+        //void getFolhaFuncionarioNome(std::string n);
         void buscaFuncionarioCodigo(int cod,int mesI);
         void buscaFuncionarioNomeEndereco(std::string BUSCA);
         void buscaIntervalo(int bmes, int bano, int bmes2, int bano2);
